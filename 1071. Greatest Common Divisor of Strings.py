34e829841c654bfa78f48aa15cb095374f61cc7d
DataScientist00 <nikunjmishra00@gmail.com>
2024-06-05 09:22:03 +0530

    Create 1071. Greatest Common Divisor of Strings.py

---
+++ 1071. Greatest Common Divisor of Strings.py
@@ -0,0 +1,12 @@
+#problem link--->> https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if len(str2) > len(str1):
+            str1 , str2 = str2 , str1
+        if str1[:len(str2)] != str2 :
+            return ""
+        if str1 == str2 :
+            return str1
+        return self.gcdOfStrings(str1[len(str2):] , str2)
+        
