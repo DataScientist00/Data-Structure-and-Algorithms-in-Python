78e2c9d9bbee397b7be1bec08a2b65133f4e5a04
DataScientist00 <nikunjmishra00@gmail.com>
2024-06-12 07:46:52 +0530

    Create 168. Excel Sheet Column Title.py

---
+++ 168. Excel Sheet Column Title.py
@@ -0,0 +1,14 @@
+#problem link--->>> https://leetcode.com/problems/excel-sheet-column-title/description/
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        res = ""
+        while columnNumber > 0:
+            offset = (columnNumber - 1) % 26
+            res += chr(ord('A') + offset)
+            columnNumber = (columnNumber - 1) // 26
+        return res[::-1]
+
+
+
+        
