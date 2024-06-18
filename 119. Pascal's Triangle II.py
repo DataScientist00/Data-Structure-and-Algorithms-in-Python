bf3cc36200b848823259e781b9e61aded1378384
DataScientist00 <nikunjmishra00@gmail.com>
2024-06-18 07:20:09 +0530

    Create 119. Pascal's Triangle II.py

---
+++ 119. Pascal's Triangle II.py
@@ -0,0 +1,14 @@
+#problem link---->> https://leetcode.com/problems/pascals-triangle-ii/description/
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        result= [1]
+        for i in range(rowIndex):
+            next_row = [0] * ( len(result)+1 )
+            for j in range(len(result)):
+                next_row[j] += result[j]
+                next_row[j+1] += result[j]
+            result = next_row
+        return result
+        
