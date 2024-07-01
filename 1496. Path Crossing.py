d6c91eb8b02305b69cc94b3794e998cb902f4642
DataScientist00 <nikunjmishra00@gmail.com>
2024-07-01 08:19:16 +0530

    Create 1496. Path Crossing.py

---
+++ 1496. Path Crossing.py
@@ -0,0 +1,25 @@
+#problem link--->>> https://leetcode.com/problems/path-crossing/description/
+
+
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        dir = {
+            "N" : [0,1],
+            "E" : [1,0],
+            "W" : [-1,0],
+            "S" : [0,-1]
+        }
+       
+        check = set()
+        x , y = 0,0
+        for c in path:
+         check.add((x,y))
+         dx , dy = dir[c]
+         x , y = x+dx , y+dy
+         if (x,y) in check:
+          return True
+        return False
+    
+        
+
+        
