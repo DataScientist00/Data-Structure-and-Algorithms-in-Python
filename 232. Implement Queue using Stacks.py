26c4ddf6d7d4d54f21937c6a91c4e0460f997231
DataScientist00 <nikunjmishra00@gmail.com>
2024-07-09 08:06:14 +0530

    Create 232. Implement Queue using Stacks.py

---
+++ 232. Implement Queue using Stacks.py
@@ -0,0 +1,40 @@
+#problemlink-->> https://leetcode.com/problems/implement-queue-using-stacks/description/
+
+
+class MyQueue:
+
+    def __init__(self):
+        self.a=[]
+        self.b=[]
+
+    def push(self, x: int) -> None:
+        self.a.append(x)
+        
+
+    def pop(self) -> int:
+        if not self.b:
+            while self.a:
+                self.b.append(self.a.pop())
+        return self.b.pop()
+            
+        
+
+    def peek(self) -> int:
+        if not self.b:
+            while self.a:
+                self.b.append(self.a.pop())
+        return self.b[-1]
+
+        
+
+    def empty(self) -> bool:
+        return max(len(self.a),len(self.b))==0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
