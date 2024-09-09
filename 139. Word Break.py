5671c0bd08b17d56d84d8781b680ccce43d9d09f
DataScientist00 <nikunjmishra00@gmail.com>
2024-09-09 19:17:28 +0530

    Update 139. Word Break.py

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -1,6 +1,6 @@
 #problem link-->> https://leetcode.com/problems/word-break/description/
 
-
+#----------Recursion-----------------
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         memo = {}
@@ -16,5 +16,22 @@ class Solution:
                         return True
             return False
         return dp(0)
+#------------bottom-up---------------
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s)+1)
+        dp[len(s)] = True
+        for i in range(len(s)-1 , -1 ,-1):
+            for c in wordDict:
+                if i + len(c) <= len(s) and s[i:i+len(c)] == c:
+                    dp[i] = dp[i+len(c)]
+                    if dp[i]:
+                        break
+        return dp[0]
+                        
+
+        
+
+        
 
         
