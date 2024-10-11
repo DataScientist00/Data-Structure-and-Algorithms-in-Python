10862f99830f11477706b684a26792bd343d08cc
DataScientist00 <nikunjmishra00@gmail.com>
2024-10-11 07:20:08 +0530

    Create 2769. Find the Maximum Achievable Number.py

---
+++ 2769. Find the Maximum Achievable Number.py
@@ -0,0 +1,7 @@
+#problem link-->> https://leetcode.com/problems/find-the-maximum-achievable-number/
+
+
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + 2* t
+        
