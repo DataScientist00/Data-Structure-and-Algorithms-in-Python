a5af2fbbf2119c5ffb801ed1e9521fdb0c822329
DataScientist00 <nikunjmishra00@gmail.com>
2024-12-27 10:20:21 +0530

    Create 268. Missing Number.py

---
+++ 268. Missing Number.py
@@ -0,0 +1,10 @@
+#problem link-->> https://leetcode.com/problems/missing-number/
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+        for i in range(len(nums)):
+            res ^= i ^ nums[i]
+        return res
+        
