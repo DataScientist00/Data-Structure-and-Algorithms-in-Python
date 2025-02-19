b0c9c7f44d27a94adc2f0446797d1ba3c9209d87
DataScientist00 <nikunjmishra00@gmail.com>
2025-02-19 08:16:55 +0530

    Create 523. Continuous Subarray Sum.py

---
+++ 523. Continuous Subarray Sum.py
@@ -0,0 +1,15 @@
+#problem link-->> https://leetcode.com/problems/continuous-subarray-sum/description/
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        rem = { 0 : -1}
+        total = 0
+        for i , n in enumerate(nums):
+            total += n
+            r = total % k
+            if r not in rem:
+                rem[r] = i
+            elif i - rem[r] > 1:
+                return True
+        return False
+        
