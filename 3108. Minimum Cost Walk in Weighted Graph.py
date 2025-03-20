9dab3744cf59edd79a6fc3ba5ddd935112fa3412
DataScientist00 <nikunjmishra00@gmail.com>
2025-03-20 10:37:22 +0530

    Create 3108. Minimum Cost Walk in Weighted Graph.py

---
+++ 3108. Minimum Cost Walk in Weighted Graph.py
@@ -0,0 +1,50 @@
+#problem link-->> https://leetcode.com/problems/minimum-cost-walk-in-weighted-graph/description/
+
+class unionfind:
+    def __init__(self , n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+    
+    def find(self , x):
+        if x!=self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self , x ,y):
+        x= self.find(x)
+        y = self.find(y)
+        if x != y:
+            if self.size[x] < self.size[y]:
+                self.parent[x] = y
+                self.size[y] += self.size[x]
+            else:
+                self.parent[y] = x
+                self.size[x] += self.size[y]
+
+class Solution:
+    def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
+        # build components
+        uf = unionfind(n)
+        for u , v, w in edges:
+            uf.union(u,v)
+        
+        # Get cost of each component
+        component_cost = {}
+        for u , v, w in edges:
+            root = uf.find(u)
+            if root not in component_cost:
+                component_cost[root] = w
+            else:
+                component_cost[root] &= w
+
+        # Queries
+        res = []
+        for s ,d in query:
+            r1, r2 = uf.find(s) , uf.find(d)
+            if r1 != r2:
+                res.append(-1)
+            else:
+                res.append(component_cost[r1])
+
+        return res
+        
