eb5aadb73f513607ed713aa803c69b3ca92c4c87
DataScientist00 <nikunjmishra00@gmail.com>
2025-04-23 07:48:27 +0530

    Create 1129. Shortest Path with Alternating Colors.py

---
+++ 1129. Shortest Path with Alternating Colors.py
@@ -0,0 +1,37 @@
+# problem link-->> https://leetcode.com/problems/shortest-path-with-alternating-colors/description/
+
+
+class Solution:
+    def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
+
+        red = defaultdict(list)
+        blue = defaultdict(list)
+
+        for src , dst in redEdges:
+            red[src].append(dst)
+        for src , dst in blueEdges:
+            blue[src].append(dst)
+
+        answer = [-1 for i in range(n)]
+        q = deque()
+        q.append([0 ,0 ,None]) # [node , length , prev_edge_color]
+        visit = set()
+        visit.add((0,None))
+
+        while q:
+            node , length , edgecolor = q.popleft()
+            if answer[node] == -1:
+                answer[node] = length
+
+            if edgecolor != "RED":
+                for nei in red[node]:
+                    if (nei , "RED") not in visit:
+                        visit.add((nei , "RED"))
+                        q.append([nei , length + 1 , "RED"])
+
+            if edgecolor != "BLUE":
+                for nei in blue[node]:
+                    if (nei , "BLUE") not in visit:
+                        visit.add((nei , "BLUE"))
+                        q.append([nei , length + 1 , "BLUE"])
+        return answer
