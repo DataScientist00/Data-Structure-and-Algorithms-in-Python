fe5f4e411ab47e789ca7c4b81f77d006320d5bbf
DataScientist00 <nikunjmishra00@gmail.com>
2025-05-07 08:22:39 +0530

    Create 1443. Minimum Time to Collect All Apples in a Tree.py

---
+++ 1443. Minimum Time to Collect All Apples in a Tree.py
@@ -0,0 +1,23 @@
+# problem link-->> https://leetcode.com/problems/minimum-time-to-collect-all-apples-in-a-tree/description/
+
+def dfs(v,gp,parent,hasApple):
+    vt=0
+    for i in range(len(gp[v])):
+        if gp[v][i]!=parent:
+            t=dfs(gp[v][i],gp,v,hasApple)
+            if hasApple[gp[v][i]] or t>0:
+                vt+=2+t
+    return vt
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        gp=[]
+        for i in range(n):
+            gp.append([])
+        for i in range(len(edges)):
+            gp[edges[i][0]].append(edges[i][1])
+            gp[edges[i][1]].append(edges[i][0])
+        return dfs(0,gp,None,hasApple)
+
+
+        # Note :- Visit this problem again ...still it's not clear
+        
