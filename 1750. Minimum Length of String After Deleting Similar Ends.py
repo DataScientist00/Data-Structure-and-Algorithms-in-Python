1bbb987ba8e328d61f49f492c064de3a88369ab6
DataScientist00 <nikunjmishra00@gmail.com>
2025-07-08 07:50:18 +0530

    Create 1750. Minimum Length of String After Deleting Similar Ends.py

---
+++ 1750. Minimum Length of String After Deleting Similar Ends.py
@@ -0,0 +1,13 @@
+# problem link-->> https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/description/
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        l , r = 0 , len(s) - 1
+        while l < r and s[l] == s[r]:
+            temp = s[l]
+            while l <= r and s[l] == temp:
+                l += 1
+            while l < r and  s[r] == temp:
+                r -= 1
+        return (r-l+1)
+        
