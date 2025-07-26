f0ac09835b9be7c4e2b04a8860a5cd768cfd8d62
DataScientist00 <nikunjmishra00@gmail.com>
2025-07-26 08:16:29 +0530

    Create 260. Single Number III.py

---
+++ 260. Single Number III.py
@@ -0,0 +1,20 @@
+
+# problem link-->> https://leetcode.com/problems/single-number-iii/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        for n in nums:
+            xor = xor ^ n
+        diff_bit = 1
+        while not(diff_bit & xor):
+            diff_bit = diff_bit << 1
+
+        a , b = 0 , 0
+        for n in nums:
+            if diff_bit & n:
+                a = a ^ n
+            else:
+                b = b ^ n
+        return [a,b]
+            
+        
