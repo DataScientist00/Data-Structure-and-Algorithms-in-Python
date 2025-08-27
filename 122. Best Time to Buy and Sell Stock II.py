aa6e40c892f8a7a0be8fde5af50870d62e1e7a87
DataScientist00 <nikunjmishra00@gmail.com>
2025-08-27 21:31:37 +0530

    Create 122. Best Time to Buy and Sell Stock II.py

---
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,29 @@
+# problem link-->> https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        memo = {}
+        def dp(i,flag):
+            if i >= len(prices):
+                return 0
+            key = (i,flag)
+            if key in memo:
+                return memo[key]
+            profit1 = profit2 = 0
+            buy = dont_buy = sell = hold = 0
+            if flag:
+                buy = - prices[i] + dp(i+1, not flag)
+                dont_buy = dp(i+1 , flag)
+                profit1 = max(buy, dont_buy)
+            else:
+                sell = prices[i] + dp(i+1 ,not flag)
+                hold = dp(i+1 ,flag)
+                profit2 = max(sell , hold)
+            memo[key] = max(profit1 ,profit2)
+            return memo[key]
+        return dp(0,1)
+
+
+
+        
