23f360da5447d6eaec688260588ae9c865c1971d
DataScientist00 <nikunjmishra00@gmail.com>
2025-08-29 12:26:51 +0530

    Create 3021. Alice and Bob Playing Flower Game.py

---
+++ 3021. Alice and Bob Playing Flower Game.py
@@ -0,0 +1,10 @@
+# problem link-->> https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+
+
+
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        return (n*m)//2
+        
+        
+        
