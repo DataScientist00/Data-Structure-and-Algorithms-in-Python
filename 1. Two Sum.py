557c39b87acf881dca6f91e0108d054b1ecef98b
DataScientist00 <nikunjmishra00@gmail.com>
2025-09-25 10:24:44 +0530

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,14 @@
+# problem link-->> https://leetcode.com/problems/two-sum/description
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hash_map:
+                return [i , hash_map[complement]]
+            hash_map[nums[i]] = i
+                
+
+        
