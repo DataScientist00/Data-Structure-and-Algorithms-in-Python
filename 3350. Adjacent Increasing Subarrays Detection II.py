0ef04c71a66816509de16b8bf2ff6b7b158206e8
DataScientist00 <nikunjmishra00@gmail.com>
2025-10-15 11:52:09 +0530

    Create 3350. Adjacent Increasing Subarrays Detection II.py

---
+++ 3350. Adjacent Increasing Subarrays Detection II.py
@@ -0,0 +1,18 @@
+# problem link-->> https://leetcode.com/problems/adjacent-increasing-subarrays-detection-ii/description/
+
+
+class Solution:
+    def maxIncreasingSubarrays(self, nums: list[int]) -> int:
+        n = len(nums)
+        up, preUp, res = 1, 0, 0
+        for i in range(1, n):
+            if nums[i] > nums[i-1]:
+                up += 1
+            else:
+                preUp = up
+                up = 1
+            half = up // 2
+            m = min(preUp, up)
+            candidate = max(half, m)
+            res = max(res , candidate)
+        return res
