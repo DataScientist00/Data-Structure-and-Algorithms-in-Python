cf019aefb117ea0d8742fd549cd546c793b501a8
DataScientist00 <nikunjmishra00@gmail.com>
2026-01-13 08:17:38 +0530

    Create 412. Fizz Buzz.py

---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,22 @@
+# problem link-->> https://leetcode.com/problems/fizz-buzz/description/?cong=true
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = []
+        for i in range(1,n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                ans.append('FizzBuzz')
+                continue
+            if i % 3 == 0:
+                ans.append('Fizz')
+                continue
+            if i % 5 == 0:
+                ans.append('Buzz')
+                continue
+            else:
+                ans.append(str(i))
+        return ans
+            
+
+        
