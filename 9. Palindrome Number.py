ae1750123f47bce71f95ec4ae935dda063dc746e
DataScientist00 <nikunjmishra00@gmail.com>
2026-01-22 12:12:10 +0530

    Create 9. Palindrome Number.py

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,16 @@
+# problem link-->> https://leetcode.com/problems/palindrome-number/description
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        i = 0
+        j = len(x)-1
+        while i <= j:
+            if x[i] != x[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+
+        
