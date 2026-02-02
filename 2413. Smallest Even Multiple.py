159e06551341b8972b8cca0bdbdc26d47afe62b6
DataScientist00 <nikunjmishra00@gmail.com>
2026-02-02 08:00:40 +0530

    Create 2413. Smallest Even Multiple.py

---
+++ 2413. Smallest Even Multiple.py
@@ -0,0 +1,10 @@
+# problem link -- >> https://leetcode.com/problems/smallest-even-multiple/description/
+
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n%2==0:
+            return n
+        else:
+            return 2*n
+        
