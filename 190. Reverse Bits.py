dda62bbe5491c9bf20181570d4debe5f863c67f3
DataScientist00 <nikunjmishra00@gmail.com>
2026-02-16 10:42:34 +0530

    Update 190. Reverse Bits.py

--- 190. Reverse Bits.py
+++ 190. Reverse Bits.py
@@ -1,1 +1,12 @@
 # problem link -- >> https://leetcode.com/problems/reverse-bits/description/
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for _ in range(32):
+            rb = n & 1
+            ans = ans << 1
+            ans = ans | rb
+            n = n >> 1
+        return ans
+        
