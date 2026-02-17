69fbf0586c3754cea2cd005ead443b67060c455b
DataScientist00 <nikunjmishra00@gmail.com>
2026-02-17 17:22:04 +0530

    Create 401. Binary Watch.py

---
+++ 401. Binary Watch.py
@@ -0,0 +1,10 @@
+# problem link -- >> https://leetcode.com/problems/binary-watch/description/
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        times = []
+        for hour in range(12):
+            for minute in range(60):
+                if (bin(hour).count('1') + bin(minute).count('1')) == turnedOn:
+                    times.append(f"{hour}:{minute:02d}")
+        return times
