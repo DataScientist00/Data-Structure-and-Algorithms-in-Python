67752d974d948850e5b32f0362c1216766f3c5ed
DataScientist00 <nikunjmishra00@gmail.com>
2026-02-25 21:04:17 +0530

    Create 1022. Sum of Root To Leaf Binary Numbers.py

---
+++ 1022. Sum of Root To Leaf Binary Numbers.py
@@ -0,0 +1,22 @@
+# problem link -- >> https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/description/
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        def dfs(node , val):
+            if not node:
+                return 0
+            val = val * 2 + node.val
+            if not node.left and not node.right:
+                return val
+            return dfs(node.left , val) + dfs(node.right , val)
+
+        return dfs(root , 0)
+        
+        
